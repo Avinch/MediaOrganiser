3b75b03f40bc4fc80f6ae2b9c847c5214f5c5a0c
Sam Scott <sam@samjas.co.uk>
2019-12-16 11:31:01 +0000

    Add basic file scanner service

---
+++ src/MediaOrganiser/MediaOrganiser/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaOrganiser.Extensions
+{
+    public static class StringExtensions
+    {
+        private static string[] AudioFileExtensions = {"mp3", "flac"};
+        private static string[] VideoFileExtensions = { "mp4", "movs" };
+
+        public static bool PathIsAudioFile(this string path)
+        {
+            return AudioFileExtensions.Any(path.EndsWith);
+        }
+
+        public static bool PathIsVideoFile(this string path)
+        {
+            return VideoFileExtensions.Any(path.EndsWith);
+        }
+    }
+}
---
+++ src/MediaOrganiser/MediaOrganiser/Service/FileScannerService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaOrganiser.Data;
+using MediaOrganiser.Extensions;
+using MediaOrganiser.Messages;
+
+namespace MediaOrganiser.Service
+{
+    class FileScannerService
+    {
+        private DataRepository _repo;
+
+        public FileScannerService()
+        {
+            _repo = new DataRepository();
+        }
+
+        public void StartScan()
+        {
+            var librariesToScan = _repo.SelectAllLibraries();
+
+            foreach (var library in librariesToScan)
+            {
+                var allFiles = Directory.GetFiles(library, "*.*", SearchOption.AllDirectories);
+
+                foreach (var path in allFiles.Where(x => x.PathIsAudioFile()))
+                {
+                    _repo.AddAudioFile(path);
+                }
+
+                foreach (var path in allFiles.Where(x => x.PathIsVideoFile()))
+                {
+                    _repo.AddVideoFile(path);
+                }
+            }
+
+            MessengerService.Default.Send(new FileScanCompleteMessage());
+        }
+    }
+}
